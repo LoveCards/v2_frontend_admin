5b136f09df0382761e27b3ad2af728e7b6143d69
zhiguai <2635435377@qq.com>
2025-08-01 20:39:27 +0800

    修复用户信息获取方式，确保在超级管理员加载项中正确异步获取用户信息；优化路由中无需鉴权的逻辑判断

--- middleware/02.intiStore.global.ts
+++ middleware/02.intiStore.global.ts
@@ -4,13 +4,17 @@ import { useTagsStore } from "@/stores/tagsStore";
 import { useSystemStore } from '~/stores/api/admin/systemStore';
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+
+    //无需鉴权跳过
+    if (!to.path.startsWith('/apps')) return;
+
     const userStore = useUserStore();
     const systemStore = useSystemStore();
     const dashboardStore = useDashboardStore();
     const tagsStore = useTagsStore();
 
     //超级管理员加载项
-    const userInfo: any = userStore.userInfo;
+    const userInfo: any = await userStore.userInfo;
     const rolesId = JSON.parse(userInfo.roles_id);
     if (rolesId.includes(0)) {
         await systemStore.init();
